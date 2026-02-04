2b96650647e44cc7a26d4c920ed4331aeef1dfdf
Manoj Raulo <manojraulo58@gmail.com>
2026-02-04 16:08:39 +0530

    backend/db.js is modified

--- backend/db.js
+++ backend/db.js
@@ -6,17 +6,19 @@ module.exports = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         };
-        const useDBAuth = process.env.USE_DB_AUTH || false;
-        if(useDBAuth){
-            connectionParams.user = process.env.MONGO_USERNAME;
-            connectionParams.pass = process.env.MONGO_PASSWORD;
-        }
-        await mongoose.connect(
-           process.env.MONGO_CONN_STR,
-           connectionParams
-        );
+
+        // Build MongoDB URI from environment variables
+        const username = process.env.MONGO_USERNAME;
+        const password = process.env.MONGO_PASSWORD;
+        const host = 'mongo-svc';  // Kubernetes service name for MongoDB
+        const dbName = 'todo';
+
+        const uri = `mongodb://${username}:${password}@${host}:27017/${dbName}?directConnection=true`;
+
+        await mongoose.connect(uri, connectionParams);
+
         console.log("Connected to database.");
     } catch (error) {
-        console.log("Could not connect to database.", error);
+        console.error("Could not connect to database.", error);
     }
 };
